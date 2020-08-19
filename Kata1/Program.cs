788007965f5a2e6ac699583613ad73691cdfe57a
Joyce Yeung <joyce.yeung@myob.com>
2020-08-19 14:13:59 +1000

    completed step 3

--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -9,7 +9,17 @@ namespace Kata1
             Console.WriteLine("Hello World!");
             Console.WriteLine("What's your name?");
             User currenUser = new User(Console.ReadLine());
-            currenUser.greetUser();
+            string[] mentors = { "Jay", "Thi" };
+
+            if(Array.Exists(mentors, e => e == currenUser.Name))
+            {
+                currenUser.greetUser();
+            }
+            else
+            {
+                Console.WriteLine("Nah");
+            }
+
 
         }
     }
