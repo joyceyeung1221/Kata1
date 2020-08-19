7f20a9083debaff1e5a8128f8c89fe69924c3815
Joyce Yeung <joyce.yeung@myob.com>
2020-08-19 13:05:18 +1000

    added User class and completed step 2

---
+++ Kata1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            User currenUser = new User(Console.ReadLine());
+            currenUser.greetUser();
+
+        }
+    }
+}
---
+++ Kata1/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Kata1
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void greetUser()
+        {
+            Console.WriteLine($"Hi~{Name}. It's lovely to see you!!");
+        }
+    }
+}
